b8a0d2ac82e8f0a9ea765b68c3ee9d397a79d09a
abhibha gupta <abhibha1807@gmail.com>
2022-03-04 15:12:51 +0530

    added step 2 till fwd pass of model1

--- architect.py
+++ architect.py
@@ -1,7 +1,13 @@
-#implments step 1 and 2 for now.
-
 def _concat(xs):
     return torch.cat([x.view(-1) for x in xs if x is not None])
+
+
+# class GreedyDecoder(nn.Module):
+#    def __init__(self, hidden_size, output_size):
+#         super(GreedyDecoder, self).__init__()
+#         self.out = nn.Linear(hidden_size, output_size)
+#         self.softmax = nn.LogSoftmax(dim=1)
+
 
 class Architect(object):
   def __init__(self, model1, model1_mom, model1_wd, A, A_lr, A_wd, device, model2, model2_wd, model2_mom):
@@ -19,44 +25,53 @@ class Architect(object):
 
   def loss1(self,train_inputs, idxs, criterion):
     A_idx = self.A(idxs)
+    #print('A_idx:', A_idx)
     batch_loss = 0
-    for i in range(batch_size): # for each instance in batch
-      input_train = train_inputs[i][0] # input indices
-      onehot_input = torch.zeros(input_train.size(0), input_lang.n_words) #input to custom embedding layer of model 1's encoder
+    for i in range(batch_size):
+      input_train = train_inputs[i][0]
+      onehot_input = torch.zeros(input_train.size(0), vocab)
       index_tensor = input_train
       onehot_input.scatter_(1, index_tensor, 1.)
       input_train = onehot_input
+      #print(input_train.size())
       target_train = train_inputs[i][1]
-      idx = A_idx[i] # ai
+      idx = A_idx[i]
       enc_hidden, enc_outputs = self.model1.enc_forward(input_train)
       loss = self.model1.dec_forward(target_train, enc_hidden) # todo: find loss for each instnce and multiply A with the loss vec.
-      loss = loss * idx #multiplying by ai 
+      #print('loss and idx size:', loss.size(), idx.size())
+      loss = loss * idx
       batch_loss += loss 
     
     return batch_loss
 
-  # step 1
   def _compute_unrolled_enc_dec_model(self, train_inputs, model1_lr, idxs, criterion):
     batch_loss = self.loss1(train_inputs, idxs, criterion)
-    #Unrolled enc model
+    #Unrolled model
     theta_enc = _concat(self.model1.enc.parameters()).data
+    #print(theta_enc, len(theta_enc))
     try:
         moment = _concat(model1.enc_optim.state[v]['momentum_buffer'] for v in self.model1.enc.parameters()).mul_(self.model1_mom)
     except:
         moment = torch.zeros_like(theta_enc)
+    #print(moment)
     dtheta = _concat(torch.autograd.grad(batch_loss, self.model1.enc.parameters(), retain_graph = True )).data + self.model1_wd*theta_enc
+    #print(dtheta)
     # # convert to the model
     unrolled_enc = self._construct_enc_from_theta(theta_enc.sub(model1_lr, moment+dtheta))
+    #print(unrolled_enc)
 
-    #Unrolled dec model
     theta_dec = _concat(self.model1.dec.parameters()).data
+    #print(theta_dec, len(theta_dec))
     try:
         moment = _concat(model1.dec_optim.state[v]['momentum_buffer'] for v in self.model1.dec.parameters()).mul_(self.model1_mom)
     except:
         moment = torch.zeros_like(theta_dec)
+    #print(moment)
     dtheta = _concat(torch.autograd.grad(batch_loss, self.model1.dec.parameters(), retain_graph = True )).data + self.model1_wd*theta_dec
+    print(dtheta)
     # # convert to the model
     unrolled_dec = self._construct_dec_from_theta(theta_dec.sub(model1_lr, moment+dtheta))
+    #print(unrolled_dec)
 
     return unrolled_enc, unrolled_dec
 
@@ -65,7 +80,8 @@ class Architect(object):
 
     model_dict = self.model1.enc.state_dict()
 
-    enc_new = self.model1.enc.new()
+    # create the new gpt model, input_lang, hidden_size, device
+    enc_new = self.model1.enc.new(vocab)
 
     # encoder update
     params, offset = {}, 0
@@ -77,12 +93,14 @@ class Architect(object):
     assert offset == len(theta)
     model_dict.update(params)
     enc_new.load_state_dict(model_dict)
+    # print([enc_new.state_dict()])
     return enc_new
 
   def _construct_dec_from_theta(self, theta):
     model_dict = self.model1.dec.state_dict()
 
-    dec_new = self.model1.dec.new()
+    # create the new gpt model, input_lang, hidden_size, device
+    dec_new = self.model1.dec.new(vocab)
 
     # encoder update
     params, offset = {}, 0
@@ -94,47 +112,58 @@ class Architect(object):
     assert offset == len(theta)
     model_dict.update(params)
     dec_new.load_state_dict(model_dict)
+    # print([enc_new.state_dict()])
     return dec_new
 
   def loss2(self, un_inputs):
     batch_loss = 0
 
-    for i in range(batch_size):  # for each instance in a batch
+    for i in range(batch_size):  
       input_un = un_inputs[i][0]
-      decoder_input = torch.tensor([[SOS_token]], device='cpu')
+      decoder_input = torch.tensor([[SOS_token]], device='cpu')#where to put SOS_token
       decoder_hidden = self.model1.dec.initHidden()
-      print('forward pass through decoder to gnerate pseudo targets')
+      print('forward pass through decoder')
      
-      ip_encoder = []   
-      pseudo_target = []
+      
+      dec_soft_idxs = []
+      decoder_outputs = []
       for di in range(MAX_LENGTH):
           decoder_output, decoder_hidden = self.model1.dec(
               decoder_input, decoder_hidden)
           topv, topi = decoder_output.topk(1)
           decoder_input = topi.squeeze().detach()  # detach from history as input
-          print('decoder output:', decoder_output.size())
-          one_hot = F.gumbel_softmax(decoder_output, tau=1, hard=True) # gumbel softmax (will change this)
-          ip_encoder.append(one_hot)
-          pseudo_target.append(torch.unsqueeze(torch.argmax(decoder_output), dim = -1)) #converting to pseudo target indices by performing argmax on output probabilities 
+          #print('decoder output:', decoder_output.size())
+          dec_soft_idx, dec_idx = torch.max(decoder_output, dim = -1, keepdims = True)
+          dec_soft_idxs.append(dec_soft_idx)
+          print('dec soft idx size:', dec_soft_idx)
+          #print(dec_soft_idxs)
+          decoder_outputs.append(torch.unsqueeze(torch.argmax(decoder_output), dim = -1))
 
           if decoder_input.item() == EOS_token:
               break
 
-     
-      #batch decode eng to french (prepare target for input to encoder to generate pseudo source)
-      #input_lang = french, output_lang = english
-      pseudo_input=[]
-      for i in range(len(pseudo_target)):
-        decoded_word = output_lang.index2word[int(torch.squeeze(pseudo_target[i], dim=0))] # get corrsponding english word using dictionary
-        try:
-          input_lang.addWord(decoded_word)# add eng word to french vocab
-        except:
-          print('word already present') 
-        encoded_word = input_lang.word2index[decoded_word] #convert eng word to french language indices
-        pseudo_input.append(encoded_word)
+      print(decoder_outputs) #pseudo target
+      decoder_outputs = torch.stack(decoder_outputs)
 
-     
+      #print(decoder_outputs.size())
+      
+      onehot_input = torch.zeros(decoder_outputs.size(0), vocab)
+      #print(onehot_input.size())
+      index_tensor = decoder_outputs
+      #print(index_tensor.size())
+      dec_soft_idxs = (torch.stack(dec_soft_idxs))
+      onehot_input = onehot_input.scatter_(1, index_tensor, 1.).float().detach() + (dec_soft_idxs).sum() - (dec_soft_idxs).sum().detach()
+      print(onehot_input.size(), onehot_input[0])
 
+      enc_hidden, enc_outputs = self.model1.enc_forward(onehot_input)
+      
+      pseudo_target = decoder_outputs
+      pseudo_input = enc_outputs
+
+      print('pseudo target:', pseudo_target, pseudo_target.size()) # words (10) x 1 (index)
+      print('pseudo input:', pseudo_input, pseudo_input.size()) # size = words (10) x hidden_size (256)
+      
+     
 
 
   def _compute_unrolled_model2(self, un_inputs, unrolled_enc, unrolled_dec, idxs, model2_lr, model2_optim):
@@ -142,15 +171,14 @@ class Architect(object):
 
 
       
-  def step(self, train_inputs, un_inputs ,model1_lr, A, idxs, criterion, model2_lr, model2_optim):
+  def step(self, train_inputs, model1_lr, A, idxs, criterion, model2_lr, model2_optim):
       self.A_optim.zero_grad()
-      #step 1
       unrolled_enc, unrolled_dec = self._compute_unrolled_enc_dec_model(train_inputs, model1_lr, idxs, criterion)
       device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
       unrolled_enc.to(device)
       unrolled_enc.train()
       unrolled_dec.to(device)
       unrolled_dec.train()
-      #step 2
-      unrolled_model2 = self._compute_unrolled_model2(un_inputs, unrolled_enc, unrolled_dec, idxs, model2_lr, model2_optim)
+      #replace unlabled dataset
+      unrolled_model2 = self._compute_unrolled_model2(train_inputs, unrolled_enc, unrolled_dec, idxs, model2_lr, model2_optim)
 
