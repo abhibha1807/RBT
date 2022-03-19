39cc3bad75c31e86fa474bceb50465201203946b
abhibha gupta <abhibha1807@gmail.com>
2022-03-19 19:24:56 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -1,25 +1,37 @@
 from Enc_Dec import *
 from dataset import *
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class Embedding_(nn.Module):
+  def __init__(self, embedding_layer):
+    super(Embedding_, self).__init__()
+    self.embedding = embedding_layer
 
+  def forward(self, mask):
+  
+    # if mask.ndim == 1:
+    if  mask.dtype == torch.long:
+        return self.embedding(mask)
+ 
+    else:
+      return torch.matmul(mask, self.embedding.weight)
 class Model1(nn.Module):
   def __init__(self, input_size, output_size, criterion, enc_hidden_size=256, dec_hidden_size=256):
     super(Model1, self).__init__()
     self.enc = EncoderRNN(input_size, enc_hidden_size)
-    #self.dec = DecoderRNN(dec_hidden_size, output_size)
     self.dec = AttnDecoderRNN(dec_hidden_size, output_size)
     self.criterion = criterion
+    self.embedding = Embedding_(self.enc.embedding)
 
   def enc_forward(self, input):
- 
+   
     encoder_hidden = self.enc.initHidden()
     input_length = input.size(0)
-    encoder_outputs = torch.zeros(input_length, self.enc.hidden_size, device=device)# how to pass max_length
+    encoder_outputs = torch.zeros(input_length, self.enc.hidden_size, device=device)
     
     for ei in range(input_length):
-      # print('input_ei:', input[ei])
+      embedded = self.embedding(input[ei]).view(1, 1, -1)
       encoder_output, encoder_hidden = self.enc(
-          input[ei], encoder_hidden)
+          embedded, encoder_hidden)
       encoder_outputs[ei] = encoder_output[0, 0]
     
     return encoder_hidden, encoder_outputs
@@ -28,31 +40,32 @@ class Model1(nn.Module):
   def dec_forward(self, target, encoder_hidden, encoder_outputs):
 
     target_length = target.size(0)
-    decoder_input = torch.tensor([[SOS_token]], device=device) #where to put SOS_token
+    decoder_input = torch.tensor([[SOS_token]], device=device) 
     decoder_hidden = encoder_hidden
+   
     loss = 0
     for di in range(target_length):
         decoder_output, decoder_hidden, decoder_attention = self.dec(
             decoder_input, decoder_hidden, encoder_outputs)
         topv, topi = decoder_output.topk(1)
         decoder_input = topi.squeeze().detach()  # detach from history as input
+      
         loss += self.criterion(decoder_output, target[di])
         if decoder_input.item() == EOS_token:
             break
-    return loss
+    return loss/target_length
 
   def new(self, vocab):
     new = Model1(vocab, vocab, self.criterion).to(device)
-
+   
     new.load_state_dict(self.state_dict())
+    #print('after loading:', dec_new)
     return new
 
   def generate(self, input, tokenizer, vocab):
     print('generating')
-    onehot_input = torch.zeros(input.size(0), vocab,  device='cuda')
-    index_tensor = input
-    onehot_input.scatter_(1, index_tensor, 1.)
-    input_train = onehot_input
+   
+    input_train = input
     enc_hidden, enc_outputs = self.enc_forward(input_train)
     decoder_input = torch.tensor([[SOS_token]], device=device) #where to put SOS_token
     decoder_hidden = enc_hidden
