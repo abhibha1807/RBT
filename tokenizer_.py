62fa12ffa15d1976356385a6d0956ff12bd882d4
abhibha gupta <abhibha1807@gmail.com>
2022-03-25 23:10:53 +0530

    Update tokenizer_.py

--- tokenizer_.py
+++ tokenizer_.py
@@ -2,6 +2,10 @@ import sys
 import os
 import json
 from tokenizers import BertWordPieceTokenizer
+from tokenizers import Tokenizer
+from tokenizers.models import  WordLevel
+
+from transformers import BertTokenizer
 from tokenizers.processors import BertProcessing
 import argparse
 
@@ -11,9 +15,8 @@ def train_tokenizer(dataset, max_length, min_freq, vocabsize, save_location):
     Train a BertWordPieceTokenizer with the specified params and save it
     """
 
-    print('is the tokenizer trained right??')
-    print(dataset[0:2])
-    # Get tokenization params
+   
+    
     save_location = save_location
     max_length = max_length
     min_freq = min_freq
@@ -27,6 +30,7 @@ def train_tokenizer(dataset, max_length, min_freq, vocabsize, save_location):
     tokenizer._tokenizer.post_processor = BertProcessing(("[SEP]", tokenizer.token_to_id("[SEP]")), ("[CLS]", tokenizer.token_to_id("[CLS]")),)
     tokenizer.enable_truncation(max_length=max_length)
     tokenizer.enable_padding(length=max_length, pad_token='[PAD]')
+    print(tokenizer.get_vocab())
 
 
     print("Saving tokenizer ...")
@@ -37,8 +41,6 @@ def train_tokenizer(dataset, max_length, min_freq, vocabsize, save_location):
 
 def get_tokenizer(pairs, max_length, min_freq, vocabsize, save_location):
     print('training tokenizers')    
-
+  
     tokenizer = train_tokenizer(pairs,  max_length, min_freq, vocabsize, save_location)
     return tokenizer
-
-
