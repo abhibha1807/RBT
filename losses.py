6a4f1748df69ff88aab3aa3165333ef28fec3cd4
abhibha gupta <abhibha1807@gmail.com>
2022-03-25 23:05:29 +0530

    Update losses.py

--- losses.py
+++ losses.py
@@ -6,40 +6,34 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def loss1(inputs, model, idxs, A, batch_size, vocab):
     A_idx = A(idxs)
-    print('A_idx:', A_idx)
     batch_loss = 0
-    print('batch size:', inputs.size(0))
     for i in range(inputs.size(0)):
         # try:
         input_train = inputs[i][0]
+       
         target_train = inputs[i][1]
         idx = A_idx[i]
         enc_hidden, enc_outputs = model.enc_forward(input_train)
         loss = model.dec_forward(target_train, enc_hidden, enc_outputs) # todo: find loss for each instnce and multiply A with the loss vec.
-        print('loss and idx :', loss, idx)
         loss = loss * idx
         batch_loss += loss 
-    print('batch loss loss1:',batch_loss)
-    return batch_loss
+    return batch_loss/inputs.size(0)
      
-        
     
-
+    
 def loss2(un_inputs, model1, model2, batch_size, vocab):
-    print('in loss 2')
     batch_loss = 0
     
     #generate pseudo target by passing through decoder
     for i in range(batch_size):  
         # try:
             input_un = un_inputs[i][0]
+            
             enc_hidden, enc_outputs = model1.enc_forward(input_un)
-            print('enc hidden:', enc_hidden)
-            print('enc_outputs:', enc_outputs)
 
             decoder_input = torch.tensor([[SOS_token]], device=device)#where to put SOS_token
             decoder_hidden = enc_hidden
-            print('forward pass through decoder')
+           # print('forward pass through decoder')
             
             dec_soft_idxs = []
             decoder_outputs = []
@@ -50,33 +44,32 @@ def loss2(un_inputs, model1, model2, batch_size, vocab):
                 decoder_input = topi.squeeze().detach()  # detach from history as input
                 dec_soft_idx, dec_idx = torch.max(decoder_output, dim = -1, keepdims = True)
                 dec_soft_idxs.append(dec_soft_idx) #save differentiable outputs
-              
+                #print(dec_soft_idxs)
                 decoder_outputs.append(torch.unsqueeze(torch.argmax(decoder_output), dim = -1))
                 if decoder_input.item() == EOS_token:
                     break
 
             #print(decoder_outputs) #pseudo target
-            print('before dec_soft_idxs:', dec_soft_idxs)# every tensor has grad fun assopciated with it.
             decoder_outputs = torch.stack(decoder_outputs)#differentiable,no break in computation graph
 
-            print('decoder_outputs:',decoder_outputs)
 
             # gumbel softmax (prepare target for generating pseudo input using encoder)
             onehot_input_encoder1 = torch.zeros(decoder_outputs.size(0), vocab, device = device)
             index_tensor = decoder_outputs
+            #print(index_tensor)
             dec_soft_idxs = (torch.stack(dec_soft_idxs))
-            print('dec_soft_idxs:', dec_soft_idxs)
             onehot_input_encoder1 = onehot_input_encoder1.scatter_(1, index_tensor, 1.).float().detach() + (dec_soft_idxs).sum() - (dec_soft_idxs).sum().detach()
-            print('onehot_input_encoder1:', onehot_input_encoder1)
 
             enc_hidden_, enc_outputs_ = model1.enc_forward(onehot_input_encoder1)
+           
             
             pseudo_target = decoder_outputs
 
+            # print('pseudo target:', pseudo_target, pseudo_target.size())
             # greedy decoding -> similar to model.generate() (hugging face)
-            decoder_input = torch.tensor([[SOS_token]], device=device)
+            decoder_input = torch.tensor([[SOS_token]], device=device)#where to put SOS_token
             decoder_hidden = enc_hidden_
-            print('decoder_hidden:', decoder_hidden)
+           
 
             dec_soft_idxs = []
             decoder_outputs = []
@@ -103,13 +96,11 @@ def loss2(un_inputs, model1, model2, batch_size, vocab):
 
             #model2 forward pass
             enc_hidden, enc_outputs = model2.enc_forward(pseudo_input)
-            loss = model2.dec_forward(pseudo_target, enc_hidden, enc_outputs) 
-            print('loss:', loss)
+            loss = model2.dec_forward(pseudo_target, enc_hidden, enc_outputs)
             batch_loss += loss 
 
             del onehot_input_encoder1 , onehot_input_model2
                 
             gc.collect()   
-    return batch_loss
+    return batch_loss/batch_size
       
-
