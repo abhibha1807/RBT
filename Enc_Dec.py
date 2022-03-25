4268a83fa9824db6ec2b74db50a3cf895c48e964
abhibha gupta <abhibha1807@gmail.com>
2022-03-25 23:00:44 +0530

    Update Enc_Dec.py

--- Enc_Dec.py
+++ Enc_Dec.py
@@ -25,7 +25,10 @@ class EncoderRNN(nn.Module):
         return output, hidden
 
     def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device='cuda')
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+      
+
 
 
 
@@ -58,15 +61,11 @@ class AttnDecoderRNN(nn.Module):
         output = torch.cat((embedded[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
 
-        output = F.relu(output)
+        #output = F.relu(output)
         output, hidden = self.gru(output, hidden)
 
         output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights
 
     def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device='cuda')
-
-  
-
-    
+        return torch.zeros(1, 1, self.hidden_size, device=device)
