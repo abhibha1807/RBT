e8fcaef13efeb78fd04581476bbc6b48a6b0475d
abhibha gupta <abhibha1807@gmail.com>
2022-03-29 11:02:49 +0530

    Update Enc_Dec.py

--- Enc_Dec.py
+++ Enc_Dec.py
@@ -29,7 +29,27 @@ class EncoderRNN(nn.Module):
 
       
 
+#decoder class
+# class DecoderRNN(nn.Module):
+#     def __init__(self, hidden_size, output_size):
+#         super(DecoderRNN, self).__init__()
+#         self.hidden_size = hidden_size
+#         self.embedding = nn.Embedding(output_size, hidden_size)
+#         self.gru = nn.GRU(hidden_size, hidden_size)
+#         self.out = nn.Linear(hidden_size, output_size)
+#         self.softmax = nn.LogSoftmax(dim=1)
 
+        
+
+#     def forward(self, embedded, hidden):
+#         #embedded = self.embedding(input).view(1, 1, -1)
+#         output = F.relu(embedded)
+#         output, hidden = self.gru(output, hidden)
+#         output = self.softmax(self.out(output[0])) #testing  cross entropy 
+#         return output, hidden
+
+#     def initHidden(self):
+#         return torch.zeros(1, 1, self.hidden_size, device='cuda')
 
 
 class AttnDecoderRNN(nn.Module):
@@ -49,8 +69,8 @@ class AttnDecoderRNN(nn.Module):
 
         
 
-    def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
+    def forward(self, embedded, hidden, encoder_outputs):
+        #embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
 
         attn_weights = F.softmax(
@@ -61,7 +81,7 @@ class AttnDecoderRNN(nn.Module):
         output = torch.cat((embedded[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
 
-        #output = F.relu(output)
+        output = F.relu(output)
         output, hidden = self.gru(output, hidden)
 
         output = F.log_softmax(self.out(output[0]), dim=1)
