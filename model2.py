95994b13a780fd854ba2db37f9bd809d64042747
abhibha gupta <abhibha1807@gmail.com>
2022-04-25 16:27:44 +0530

    Update model2.py

--- model2.py
+++ model2.py
@@ -62,26 +62,30 @@ class Model2(nn.Module):
 
 
   def enc_forward(self, input):
-  
+    #print('forward pass through encoder')
+    #print(input, input.size())
     encoder_hidden = self.enc.initHidden()
     input_length = input.size(0)
     encoder_outputs = torch.zeros(input_length, self.enc.hidden_size, device=device)# how to pass max_length
     
     for ei in range(input_length):
-     
+      # print('input_ei:', input[ei])
+      # inp_emb = self.embedding(input_ids)/self.enc_emb_scale
       embedded = self.embedding_enc(input[ei]).view(1, 1, -1)
       embedded = embedded/math.sqrt(self.enc_hidden_size)
       encoder_output, encoder_hidden = self.enc(embedded, encoder_hidden)
       encoder_outputs[ei] = encoder_output[0, 0]
     
+    #print(encoder_hidden.size(),encoder_outputs.size())
     return encoder_hidden, encoder_outputs
 
   
   def dec_forward(self, target, encoder_hidden, encoder_outputs):
     #print('forward pass through decoder')
-  
+    # print('target size:', target.size())
     target_length = target.size(0)
-    decoder_input = torch.tensor([[SOS_token]], device=device) 
+    #print(target)
+    decoder_input = torch.tensor([[SOS_token]], device=device) #where to put SOS_token
     decoder_hidden = encoder_hidden
     loss = 0
     for di in range(target_length):
@@ -91,8 +95,10 @@ class Model2(nn.Module):
             embedded, decoder_hidden)
         topv, topi = decoder_output.topk(1)
         decoder_input = topi.squeeze().detach()  # detach from history as input
+        print('decoder output model2:', decoder_output, target[di].size())
         #decoder_input = target[di]  #teacher forcing
         loss += self.criterion(decoder_output, target[di])
+        print('calculated loss grad fn:', loss, loss.grad_fn)
         if decoder_input.item() == EOS_token:
             break
     return loss/target_length
@@ -125,7 +131,9 @@ class Model2(nn.Module):
         outputs.append(int(index))
         if decoder_input.item() == EOS_token:
             break
-   
+    # outputs = torch.stack(outputs)
+    #print(outputs)
     decoded_sentence = tokenizer.decode(outputs)
+    #print(decoded_sentence)
     return decoded_sentence
 
